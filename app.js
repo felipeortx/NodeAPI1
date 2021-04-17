70a52f00950bfcbf92de7efadbdfa4abebd984ea
felipeortx <82383512+felipeortx@users.noreply.github.com>
2021-04-17 19:15:29 -0300

    first api node

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 const products = [
-    {
-      id: 1,
-      name: "hammer",
-    },
-    {
-      id: 2,
-      name: "screwdriver",
-    },
-    ,
-    {
-      id: 3,
-      name: "wrench",
-    },
-   ];
+  {
+    id: 1,
+    name: "hammer",
+  },
+  {
+    id: 2,
+    name: "screwdriver",
+  },
+  {
+    id: 3,
+    name: "wrench",
+  },
+];
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
 
-app.get("/products", (req, res) => {  
-   res.json(products);
-  });
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/products/:id", (req, res) => {
-    console.log("passei aqui");
-    const pds = products.filter(p => p.id.toString() === req.params.id);
-    res.json(pds);
+app.get("/products", (req, res) => {
+  res.json(products);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/products/:id", (req, res) => {
+  const pds = products.filter((p) => p.id.toString() === req.params.id);
+  res.json(pds);
+});
+
+app.post("/products", (req, res) => {
+  const newProjet = { id: products.length + 1, ...req.body };
+  const newProducts = [...products, newProjet];
+  res.json(newProducts);
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
